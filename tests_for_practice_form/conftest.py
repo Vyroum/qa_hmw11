7bcc1c992c9a25fb31425e0179ca6a7bb7e41f40
Andrei Monichev <vyroud@gmail.com>
2024-12-15 19:36:26 +0300

    added tests for Jenkins

--- tests_for_practice_form/conftest.py
+++ tests_for_practice_form/conftest.py
@@ -9,7 +9,7 @@ def browser_set():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "126.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
